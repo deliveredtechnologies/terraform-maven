c3f4265f1108c7162a00f785ed7c1f9404c2fd02
maganti <maganti.0792@gmail.com>
2020-04-23 16:11:06 -0500

    test cases for TerraformUploadTest

--- tf-build-tools/tf-maven-plugin/src/test/java/com/deliveredtechnologies/maven/terraform/TerraformUploadTest.java
+++ tf-build-tools/tf-maven-plugin/src/test/java/com/deliveredtechnologies/maven/terraform/TerraformUploadTest.java
@@ -1,16 +1,47 @@
 package com.deliveredtechnologies.maven.terraform;
 
+import com.deliveredtechnologies.maven.terraform.TerraformUpload.TerraformUploadParams;
 import com.deliveredtechnologies.io.Executable;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.shared.invoker.InvocationRequest;
+import org.apache.maven.shared.invoker.Invoker;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
+import org.apache.maven.plugin.logging.Log;
 
 import java.io.IOException;
+import java.util.Properties;
 
 public class TerraformUploadTest {
 
+//  private MavenProject project;
+  private Log log;
+  private Properties properties;
+//  private TerraformUpload terraformUpload;
+//  private Invoker invoker;
+//  private InvocationRequest request;
+
+
+  @Before
+  public void setup() {
+//    this.project = new MavenProject();
+    this.log = Mockito.mock(Log.class);
+    this.properties = Mockito.spy(Properties.class);
+//    this.terraformUpload = new TerraformUpload(properties);
+  }
+
   @Test
   public void terraformUploadCliUploadsToS3WithKmsEncryption() throws IOException, InterruptedException {
+    String planOutputFile = "test.json";
+    String sse  = "sse";
+    String kmsKeyId = "1234";
+
+    properties.put(TerraformUploadParams.kmsKeyId.toString(),kmsKeyId);
+    properties.put(TerraformUploadParams.sse.toString(),sse);
+    properties.put(TerraformUploadParams.planOutputFile.toString(),planOutputFile);
+
     Executable executable = Mockito.mock(Executable.class);
     Logger logger = Mockito.mock(Logger.class);
 
@@ -24,6 +55,15 @@ public class TerraformUploadTest {
 
   @Test
   public void terraformUploadCliUploadsToS3WithNoEncryption() throws IOException, InterruptedException {
+
+    String planOutputFile = "test.json";
+    String sse  = "AES:256";
+    String kmsKeyId = "1234";
+
+    properties.put(TerraformUploadParams.kmsKeyId.toString(),kmsKeyId);
+    properties.put(TerraformUploadParams.sse.toString(),sse);
+    properties.put(TerraformUploadParams.planOutputFile.toString(),planOutputFile);
+
     Executable executable = Mockito.mock(Executable.class);
     Logger logger = Mockito.mock(Logger.class);
 
