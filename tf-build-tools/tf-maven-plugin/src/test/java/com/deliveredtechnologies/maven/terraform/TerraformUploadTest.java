83971831c7244ea6e628bea47279a43ad7931034
maganti <maganti.0792@gmail.com>
2020-04-23 16:29:58 -0500

    removed unused imports & code

--- tf-build-tools/tf-maven-plugin/src/test/java/com/deliveredtechnologies/maven/terraform/TerraformUploadTest.java
+++ tf-build-tools/tf-maven-plugin/src/test/java/com/deliveredtechnologies/maven/terraform/TerraformUploadTest.java
@@ -2,38 +2,26 @@ package com.deliveredtechnologies.maven.terraform;
 
 import com.deliveredtechnologies.maven.terraform.TerraformUpload.TerraformUploadParams;
 import com.deliveredtechnologies.io.Executable;
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.shared.invoker.InvocationRequest;
-import org.apache.maven.shared.invoker.Invoker;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.slf4j.Logger;
-import org.apache.maven.plugin.logging.Log;
 
 import java.io.IOException;
 import java.util.Properties;
 
 public class TerraformUploadTest {
 
-//  private MavenProject project;
-  private Log log;
   private Properties properties;
-//  private TerraformUpload terraformUpload;
-//  private Invoker invoker;
-//  private InvocationRequest request;
-
 
   @Before
   public void setup() {
-//    this.project = new MavenProject();
-    this.log = Mockito.mock(Log.class);
     this.properties = Mockito.spy(Properties.class);
-//    this.terraformUpload = new TerraformUpload(properties);
   }
 
   @Test
   public void terraformUploadCliUploadsToS3WithKmsEncryption() throws IOException, InterruptedException {
+
     String planOutputFile = "test.json";
     String sse  = "sse";
     String kmsKeyId = "1234";
