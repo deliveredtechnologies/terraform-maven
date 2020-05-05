07b0cddd4c38097191f406013e291fbb9fe4b504
Jeff Lanka <jefflanka@yahoo.com>
2020-05-05 12:43:42 -0400

    Clayton's modifications

--- tf-build-tools/tf-maven-plugin/src/test/java/com/deliveredtechnologies/maven/terraform/mojo/WrapperTest.java
+++ tf-build-tools/tf-maven-plugin/src/test/java/com/deliveredtechnologies/maven/terraform/mojo/WrapperTest.java
@@ -1,5 +1,16 @@
 package com.deliveredtechnologies.maven.terraform.mojo;
 
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -8,26 +19,22 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
-import com.deliveredtechnologies.terraform.TerraformException;
-import org.apache.commons.io.FileUtils;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.shared.invoker.Invoker;
-import org.apache.maven.shared.invoker.MavenInvocationException;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mockito;
+//import static org.junit.Assert.assertEquals;
+//import static org.mockito.Mockito.*;
 
-
+/**
+ * WrapperTest class.
+ *
+ */
 public class WrapperTest {
   Log log;
   Wrapper wrapper;
 
+  /**
+   * WrapperTest class.
+   *
+   */
   @Before
   public void setup() {
     log = Mockito.mock(Log.class);
@@ -37,7 +44,7 @@ public class WrapperTest {
 
 
   @Test
-  public void TryHappyWrapper() throws IOException, MojoFailureException, MojoExecutionException {
+  public void tryHappyWrapper() throws IOException, MojoFailureException, MojoExecutionException {
     wrapper.execute();
   }
 
@@ -79,42 +86,7 @@ public class WrapperTest {
 
   }
 
-  /*
-  @Test
-  public void FetchPropertiesAndCheck() throws IOException, MojoExecutionException, MojoFailureException {
-    Log log = Mockito.mock(Log.class);
-    Invoker invoker = Mockito.mock(Invoker.class);
-    Wrapper wrapper = new Wrapper();
-    wrapper.execute();
-    Properties prop = new Properties();
-    File propFile = new File(System.getProperty("user.dir") + "\\.tf\\terraform-maven.properties");
-    InputStream fis = new FileInputStream(propFile);
-    prop.load(fis);
-    String releaseDir = prop.getProperty("releaseDir");
-    assertEquals("terraform",releaseDir);
-    String releaseName = prop.getProperty("releaseName");
-    assertEquals("terraform",releaseName);
-    String releaseSuffix = prop.getProperty("releaseSuffix");
-    assertEquals("amd64.zip",releaseSuffix);
-    String releaseOS = prop.getProperty("releaseOS");
-    assertEquals("windows",releaseOS);
-    String releaseVer = prop.getProperty("releaseVer");
-    assertEquals("0.12.24",releaseVer);
-    String distributionSite = prop.getProperty("distributionSite");
-    assertEquals("https://releases.hashicorp.com",distributionSite);
-  }
-  */
 
-  @Test
-  public void SetIncomingParameters() throws IOException, MojoExecutionException, MojoFailureException {
-    String indistributionSite = "https://releases.hashicorp.com";
-    String inreleaseDir = "terraform";
-    String inreleaseName = "terraform";
-    String inreleaseVer = "0.12.24";
-    String inreleaseOS = "linux";
-    String inreleaseSuffix = "amd64.zip";
-    wrapper.execute();
-  }
 
   @Test
   public void testSetup() {
