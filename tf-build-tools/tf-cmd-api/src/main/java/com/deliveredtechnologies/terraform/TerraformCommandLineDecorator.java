d12a74ecff2ee9733b4a9744a8e17d71cbddffde
Jeff Lanka <jefflanka@yahoo.com>
2020-05-29 07:46:35 -0400

    removed extra printf statements

--- tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/TerraformCommandLineDecorator.java
+++ tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/TerraformCommandLineDecorator.java
@@ -110,14 +110,12 @@ public class TerraformCommandLineDecorator implements Executable {
           tfScr = new File(String.format("%s%stfw", tfDir, File.separator));
         }
         if (tfScr.exists()) {
-          System.out.println(String.format("%s%s %s %s", unixPath, "/tfw", cmd.toString(), StringUtils.isEmpty(command) ? "" : command));
-          System.out.println(String.format("terraform %1$s %2$s", cmd.toString(), StringUtils.isEmpty(command) ? "" : command));
           return String.format("%s%s %s %s", unixPath, "/tfw", cmd.toString(), StringUtils.isEmpty(command) ? "" : command);
         }
       }
     } catch (Exception e) {
       e.printStackTrace();
-      System.out.printf("Unable to create .tf directory\n");
+      System.out.printf(".tf directory does not exist, please run mvn tf:wrapper first\n");
     }
     return String.format("terraform %1$s %2$s", cmd.toString(), StringUtils.isEmpty(command) ? "" : command);
   }
