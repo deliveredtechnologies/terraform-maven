6b349191d9d6d8c82dd741941c2a9e136516eb12
maganti <maganti.0792@gmail.com>
2020-06-22 17:44:22 -0500

    test cases for TerraformChainHandler

--- tf-build-tools/tf-cmd-api/src/test/java/com/deliveredtechnologies/terraform/terraformhandler/TerraformApplyS3HandlerTest.java
+++ tf-build-tools/tf-cmd-api/src/test/java/com/deliveredtechnologies/terraform/terraformhandler/TerraformApplyS3HandlerTest.java
@@ -28,7 +28,20 @@ public class TerraformApplyS3HandlerTest {
   }
 
   @Test
-  public void planOperationWithAllPropertiesSpecified() throws IOException, InterruptedException {
+  public void initiatingTheChainFromApply() throws IOException, TerraformException, InterruptedException {
+    TerraformChainHandler terraformChainHandler = new TerraformChainHandler(tfRootDir,logger);
+    terraformChainHandler.chainInitiator(properties);
+    TerraformHandler terraformHandler = new TerraformApplyS3Handler(executable,logger);
+
+    String s3BucketKey = "s3://terraform-maven-state/planfiles/test.json";
+    properties.put("plan", s3BucketKey);
+
+    terraformHandler.doAction(properties);
+    Mockito.verify(executable, Mockito.times(1)).execute("aws s3api get-object --bucket terraform-maven-state --key planfiles/test.json test.json");
+  }
+
+  @Test
+  public void planOperationWithAllPropertiesSpecified() throws IOException, InterruptedException, TerraformException {
     TerraformHandler terraformHandler = new TerraformApplyS3Handler(executable,logger);
 
     String s3BucketKey = "s3://terraform-maven-state/planfiles/test.json";
