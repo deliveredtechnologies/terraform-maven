9f15700339d36edce3372a07fe2c392db7dfa1bb
robert <rdhutto1@gamil.com>
2020-06-23 11:26:44 -0400

    fixed unused import

--- tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/api/TerraformOperation.java
+++ tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/api/TerraformOperation.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 
 public interface TerraformOperation<T> {
