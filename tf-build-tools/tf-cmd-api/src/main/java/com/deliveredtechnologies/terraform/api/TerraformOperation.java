14fd1c2d312f4ee1b5091d8b0ed439ea9253e90a
robert <rdhutto1@gamil.com>
2020-06-23 11:05:43 -0400

    added ability to pass complex types via mvn command line and through cmd api

--- tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/api/TerraformOperation.java
+++ tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/api/TerraformOperation.java
@@ -1,8 +1,11 @@
 package com.deliveredtechnologies.terraform.api;
 
 import com.deliveredtechnologies.terraform.TerraformException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 
 public interface TerraformOperation<T> {
@@ -18,5 +21,26 @@ public interface TerraformOperation<T> {
     Properties props = new Properties();
     props.putAll(properties);
     return execute(props);
+  }
+
+  /**
+   * Converts a map to command line options.
+   * @param name option name.
+   * @param map
+   * @return
+   * @throws TerraformException
+   */
+  default StringBuilder convertMapToCommandLineOptions(String name, Map<String,Object> map) throws TerraformException {
+    ObjectMapper mapper = new ObjectMapper();
+    StringBuilder sb = new StringBuilder();
+    for (Map.Entry<String,Object> entry: map.entrySet()) {
+      Object val = entry.getValue();
+      try {
+        sb.append(String.format("-%s '%s=%s' ", name, entry.getKey(), val instanceof String ? val : mapper.writeValueAsString(val)));
+      } catch (JsonProcessingException e) {
+        throw new TerraformException(e);
+      }
+    }
+    return sb;
   }
 }
