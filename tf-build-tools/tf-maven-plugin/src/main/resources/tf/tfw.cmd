f49ebf682974b47caac23341217bd04dcb45875d
Jeff Lanka <jefflanka@yahoo.com>
2020-07-16 13:38:12 -0400

    changed line 6 to powershell -ep bypass -file %0\..\tfw.ps1 %* changed line 4 to set TARGET=%INPUT%\..

--- tf-build-tools/tf-maven-plugin/src/main/resources/tf/tfw.cmd
+++ tf-build-tools/tf-maven-plugin/src/main/resources/tf/tfw.cmd
@@ -1,8 +1,8 @@
 echo off
 set ORIG=%cd%
 set INPUT=%0%
-set TARGET=%INPUT:tfw.cmd=%
+set TARGET=%INPUT%\..
 cd %TARGET%
-powershell -ep bypass -file tfw.ps1 %*
+powershell -ep bypass -file %0\..\tfw.ps1 %*
 cd %ORIG%
 exit /b
