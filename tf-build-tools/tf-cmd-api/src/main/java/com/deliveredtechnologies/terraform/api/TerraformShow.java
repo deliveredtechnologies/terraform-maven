9c6675ef3e9d0849db4cda78b278cc6e99cb794e
Robert Hutto <roberthutto@users.noreply.github.com>
2020-08-17 21:22:41 -0400

    Feature/206 tf test (#207)
    added Example of using tf-cmd-api show command to write pre-provision test similar to sentinel using groovy and spock
    
    authored-by: robert <rdhutto1@gamil.com>

--- tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/api/TerraformShow.java
+++ tf-build-tools/tf-cmd-api/src/main/java/com/deliveredtechnologies/terraform/api/TerraformShow.java
@@ -1,12 +1,9 @@
 package com.deliveredtechnologies.terraform.api;
 
-import com.deliveredtechnologies.io.CommandLine;
 import com.deliveredtechnologies.io.Executable;
 import com.deliveredtechnologies.terraform.TerraformCommand;
 import com.deliveredtechnologies.terraform.TerraformCommandLineDecorator;
 import com.deliveredtechnologies.terraform.TerraformException;
-import com.deliveredtechnologies.terraform.TerraformUtils;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -42,18 +39,15 @@ public class TerraformShow extends TerraformCliOperation {
       return this.format;
     }
 
-
   }
 
   TerraformShow(Executable terraform) {
     super(terraform);
   }
 
-
   public TerraformShow(String tfRootDir) throws IOException, TerraformException {
-    this(new TerraformCommandLineDecorator(TerraformCommand.SHOW, new CommandLine(tfRootDir == null || tfRootDir.isEmpty() ? TerraformUtils.getDefaultTerraformRootModuleDir() : TerraformUtils.getTerraformRootModuleDir(tfRootDir), false, LoggerFactory.getLogger(CommandLine.class))));
+    this(new TerraformCommandLineDecorator(TerraformCommand.SHOW, tfRootDir));
   }
-
 
   @Override
   protected TerraformOption[] getTerraformParams() {
