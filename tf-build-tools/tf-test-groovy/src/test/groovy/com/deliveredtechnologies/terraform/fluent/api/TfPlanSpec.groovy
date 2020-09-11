28eb215a9fddd4b79e59504f9e40ccc9e1b5d491
robert <rdhutto1@gamil.com>
2020-09-11 08:43:34 -0400

    initial commmit

---
+++ tf-build-tools/tf-test-groovy/src/test/groovy/com/deliveredtechnologies/terraform/fluent/api/TfPlanSpec.groovy
@@ -0,0 +1,49 @@
+package com.deliveredtechnologies.terraform.fluent.api
+
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+class TfPlanSpec extends Specification {
+
+    @Shared
+    TfPlan tfPlan
+
+    def setupSpec() {
+        tfPlan = new TfPlan(new File("./src/test/resources/terraform.tfplan.json").text)
+    }
+
+    def "GetResourcesByType"() {
+        given:
+        List<Map> resources = tfPlan.getResourcesByType("aws_s3_bucket")
+
+        expect:
+        resources.size() == 3
+        resources[0].type == "aws_s3_bucket"
+        resources[1].type == "aws_s3_bucket"
+        resources[2].type == "aws_s3_bucket"
+
+    }
+
+    def "GetResourcesBy"() {
+        given:
+        List<Map> resources = tfPlan.getResourcesBy({it.type == "aws_s3_bucket"})
+
+        expect:
+        resources.size() == 3
+        resources[0].type == "aws_s3_bucket"
+        resources[1].type == "aws_s3_bucket"
+        resources[2].type == "aws_s3_bucket"
+    }
+
+    def "GetOutputs"() {
+        given:
+        Map outputs = tfPlan.getOutputs()
+
+        expect:
+        outputs.bucket_names.value.size() == 3
+        outputs.bucket_names.value.contains("tftest-bucket1")
+        outputs.bucket_names.value.contains("tftest-bucket2")
+        outputs.bucket_names.value.contains("tftest-bucket3")
+    }
+}
